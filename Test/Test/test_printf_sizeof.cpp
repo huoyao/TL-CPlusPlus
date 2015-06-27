031df95342d72b4aacb4150bed7dfce3ce4c27fe
huoyao <huoyaotl123@126.com>
2015-06-27 10:30:07 +0800

    printf sizeof test --add

---
+++ Test/Test/test_printf_sizeof.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void testPrintf()
+{
+  char c = -1;
+  printf("%02x %02x\n", c, (unsigned char)c);
+}
+
+void testSizeof()
+{
+  cout << "sizeof(\"\\0\")" << sizeof("\0") << endl;
+  cout << "strlen(\"\\0\")" << strlen("\0") << endl;
+  int *c = new int[10];
+  c[0] = 1;
+  cout << "sizeof(*c)" << sizeof(*c) << "  " << *c << endl;
+  delete[] c;
+}
+
+int main()
+{
+  //testPrintf();
+  //testSizeof();
+  return 0;
+}
